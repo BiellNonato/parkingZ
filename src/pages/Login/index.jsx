c432e1c8105e399b91e86c1427802daaf6feb88d
BiellNonato <gabrielnonato53@gmail.com>
2025-09-10 16:38:15 -0300

    feat: add inputs and button

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,17 +1,24 @@
-import { LoginContainer, Titulo,Image, Subtittle, InputEmail, InputSenha } from "./style";
+import { Text } from "react-native";
+import { LoginContainer, Titulo, Image, Subtittle, InputEmail, InputSenha, InputContainer, Button, TitleButton, IconsContainer, } from "./style";
 
 
 
-export default function Login(){
-    return(
-        <LoginContainer>
-          <Image source={require('../../assets/gojo.png')}></Image>
-          <Titulo>ParkingZ</Titulo>
-          <Subtittle>Login</Subtittle>
-          <InputEmail>Email:</InputEmail>
-          <InputSenha>Senha:</InputSenha>
-        </LoginContainer>
-    )
+export default function Login() {
+  return (
+    <LoginContainer>
+      <Image source={require('../../assets/gojo.png')}></Image>
+      <Titulo>ParkingZ</Titulo>
+      <Subtittle>Login</Subtittle>
+      <InputContainer>
+        <InputEmail placeholder="Email:"></InputEmail>
+        <InputSenha placeholder="Senha:"></InputSenha>
+      </InputContainer>
+      <Button>
+        <TitleButton>Entrar</TitleButton>
+      </Button>
+    </LoginContainer>
+    
+  )
 }
 
 
--- src/pages/Login/style.js
+++ src/pages/Login/style.js
@@ -2,7 +2,7 @@ import styled from "styled-components/native";
 
 export const LoginContainer = styled.View`
 display: flex;
-gap: 10;
+gap: 15;
  flex: 1;
  background-color:#454848;
  height: 100%;
@@ -13,44 +13,66 @@ justify-self: center;
 justify-content: center;
 align-self: center;
 font-size: 25;
-color: black;
+color: white;
+font-family: IrishGrover-Regular;
 `
 export const Image = styled.Image`
 margin-top: 20;
 justify-self: center;
 align-self: center;
-width: 200;
-height: 200;
+width: 300;
+height: 300;
 `
 export const Subtittle = styled.Text`
 justify-self: center;
 align-self: center;
 font-size: 20;
-color: black;
+color: white;
 `
 export const InputEmail = styled.TextInput`
 justify-self: center;
 align-self: center;
 border-radius: 10px;
-width: 350;
-height: 50;
-background-color: white;
+width: 292;
+height: 41;
+background-color:  #D9D9D9;
 color: black;
 `
 export const InputSenha = styled.TextInput`
 justify-self: center;
 align-self: center;
 border-radius: 10px;
-width: 350;
-height: 50;
-background-color: white;
+width: 292;
+height: 41;
+background-color:  #D9D9D9;
 color: black;
 `
 
-export const BntEntrar = styled.Button`
+export const Button = styled.TouchableOpacity`
+margin-top: 30px;
+justify-self: center;
+align-self: center;
+border-radius: 10px;
 width: 116;
 height: 41;
+background-color:  #D9D9D9;
 `
+
+export const  TitleButton = styled.Text`
+margin-top: 8px;
+justify-self: center;
+align-self: center;
+font-size: 15;
+color: black;
+`
+
+export const InputContainer = styled.View`
+ display: flex;
+ gap: 60;
+`
+
+
+
 
 
 
